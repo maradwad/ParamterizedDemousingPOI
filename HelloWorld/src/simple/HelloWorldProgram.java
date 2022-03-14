61fadcb828384912026eccd7bab9f717f839337d
maradwad <madhuriaradwad@gmail.com>
2022-03-14 11:40:58 +0530

    HelloWorld

---
+++ HelloWorld/src/simple/HelloWorldProgram.java
@@ -0,0 +1,10 @@
+package simple;
+
+public class HelloWorldProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		}
+
+}
